d8e936d612908136ec6c6aa6f0c05a53d88ddf6a
Jack Goza <jackgoza@gmail.com>
2018-09-14 18:55:16 -0500

    figuring out components slowly but surely. Routing? eh....

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+  BrowserModule,
+  UiModule,
+  AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
